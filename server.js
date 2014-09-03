09f12a73639e4f4cfaa27678f19db46795593c1e
Harry JUMEAU <harry.jumea@gmail.com>
2014-09-03 16:23:11 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+var app = express();
+
+app.use(express.static(__dirname));
+
+var _httpServer = http.createServer(app)
+
+var clients = [];
+
+// create the server
+wsServer = new WebSocketServer({
+    httpServer: _httpServer
+});
+
+// WebSocket server
+wsServer.on('request', function(request) {
+    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+    var connection = request.accept(null, request.origin);
+    // we need to know client index to remove them on 'close' event
+    var index = clients.push(connection) - 1;
+    // This is the most important callback for us, we'll handle
+    // all messages from users here.
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+          // process WebSocket message
+          clients.forEach(function(client){
+            client.sendUTF(message.utf8Data);
+          });
+        }
+    });
+
+    connection.on('close', function(connection) {
+        // close user connection
+        clients.splice(index, 1);
+    });
+});
+
+_httpServer.listen(5010, function(){
+  console.log("WebSocket server listening on port 5010")
+});
